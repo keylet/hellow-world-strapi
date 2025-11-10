e79547201ff3143c8439c03e5ea39ae836c1e177
keylet <alexpazos88@gmail.com>
2025-11-10 00:46:33 -0500

    first commit

---
+++ hello-world-frontend/src/App.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // Reemplaza la URL con la de tu instancia de Strapi
+    axios.get('http://localhost:1337/api/messages')
+      .then(response => {
+        setMessage(response.data.data[0].HelloWorld);
+      })
+      .catch(error => {
+        console.error('Error fetching message:', error);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message ? message : "Loading..."}</h1>
+    </div>
+  );
+}
+
+export default App;
